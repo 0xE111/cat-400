d506fcc4926c2c492f487500ac6883bb30821766
Alex Goncharov <alexandr@hobby4me.ru>
2018-02-14 01:22:44 +0300

    Wipe states from server

--- c4/server.nim
+++ c4/server.nim
@@ -11,42 +11,19 @@ type
     port: uint16,
   ]
 
-  None* = object of State
-  Loading* = object of State
-  Running* = object of State
-
   Server* = object of RootObj
-    state: ref State
     config: ServerConfig
 
-let
-  noneState = new(ref None)
-  loadingState = new(ref Loading)
-  runningState = new(ref Running)
-
-method switch(self: var ref State, newState: ref Loading, instance: ref Server) =
-  if self of ref None:
-    self = newState
-
-    logging.debug("Loading")
-    instance.config.network.init(port=instance.config.port)
-
-method switch(self: var ref State, newState: ref Running, instance: ref Server) =
-  if self of ref Loading:
-    self = newState
-    logging.debug("Running")
-
-    runLoop(
-      updatesPerSecond = 30,
-      fixedFrequencyHandlers = @[
-        proc(dt: float): bool = instance.config.network.update(dt),  # anonymous proc
-        proc(dt: float): bool = instance.state of Running,  # check whether state is 'Running'
-      ], 
-    )
 
 proc run*(self: ref Server, config: ServerConfig) =
   logging.debug("Starting server")
   self.config = config
-  self.state = noneState
-  self.state.switch(loadingState, instance=self)
-  self.state.switch(runningState, instance=self)
+
+  self.config.network.init(port=self.config.port)
+  
+  runLoop(
+    updatesPerSecond = 30,
+    fixedFrequencyHandlers = @[
+        proc(dt: float): bool = self.config.network.update(dt),  # anonymous proc
+      ],
+  )
--- c4/utils/state.nim
+++ c4/utils/state.nim
@@ -1,10 +1,12 @@
 type
-  State* = object of RootObj
+  State* = object {.inheritable.}
 
 
 method switch*(self: var ref State, newState: ref State) {.base, inline.} = discard  # TODO: use ref RootObj|void maybe?
-method switch*(self: var ref State, newState: ref State, instance: ref RootObj) {.base, inline.} = discard
+method switch*(self: var ref State, newState: ref State, instance: ref object) {.base, inline.} = discard
 ## A function to decide whether to switch to a new state.
 ## By default, calling old_state.switch(new_state) doesn't change the state.
 ## User should create his own methods to allow switching between states.
 ## Warning: calling `switch` inside `switch` may lead to stack overflow.
+
+method update*(self: var ref State, instance: ref object) {.base, inline.} = discard
