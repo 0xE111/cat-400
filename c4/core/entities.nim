bfa8be3e139c4a393ceeefaf871801e8cc5fb234
Alex Goncharov <alexandr@hobby4me.ru>
2018-03-15 14:52:00 +0300

    Add basic entities

---
+++ c4/core/entities.nim
@@ -0,0 +1,37 @@
+from tables import Table, initTable
+
+
+type
+  Entity* = int16  # uint doesn't check boundaries
+
+var
+  entities: set[Entity] = {}  # set[int32] won't compile
+
+
+# ---- Entity ----
+proc newEntity*(): Entity =
+  result = low(Entity)
+  while result in entities:
+    result += 1
+
+  entities.incl(result)  # add entity to global entities registry
+
+proc delete*(entity: Entity) =
+  entities.excl(entity)  # 2do: will it raise an exception if id not in entities?
+  # 2do: delete all components related to this entity
+
+# ---- Components ----
+proc getComponents*(t: typedesc): var Table[Entity, t] =
+  ## Returns components for all entities (Table: key=Entity, value=component)
+  var table {.global.} = initTable[Entity, t]()
+  return table
+
+# high-level entity wrappers
+proc setComponent*(entity: Entity, t: typedesc; value: t) {.inline.} = getComponents(t).add(entity, value)
+proc hasComponent*(entity: Entity, t: typedesc): bool {.inline.} = getComponents(t).hasKey(entity)
+proc getComponent*(entity: Entity, t: typedesc): var t {.inline.} = getComponents(t)[entity]
+proc deleteComponent*(entity: Entity, t: typedesc) {.inline.} = getComponents(t).del(entity)
+
+# short aliases
+proc `[]=`* (entity: Entity, t: typedesc; value: t) {.inline.} = entity.setComponent(t, value)
+proc `[]`* (entity: Entity, t: typedesc): var t {.inline.} = entity.getComponent(t)
