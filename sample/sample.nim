a15e52e20718aa216a6c7c81ba44f65d2a31f434
Alex Goncharov <alexandr@hobby4me.ru>
2018-04-12 15:59:52 +0300

    Rename sample main file; include assets

---
+++ sample/sample.nim
@@ -0,0 +1,19 @@
+import c4.config
+import c4.core
+
+import systems.physics
+import systems.input
+import systems.video
+import systems.network
+
+import core.messages
+import core.states.server
+
+
+config.title = "Sample game"
+config.version = "0.1"
+config.systems.input = new(CustomInputSystem)
+
+
+when isMainModule:
+  core.run()
