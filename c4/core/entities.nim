88f39964393ae261997c5f8eefc897a00b154579
Alex Goncharov <alexandr@hobby4me.ru>
2018-05-15 16:59:30 +0300

    Try to remove components together with an Entity (fail)

--- c4/core/entities.nim
+++ c4/core/entities.nim
@@ -1,6 +1,6 @@
 ## This module defines Entity - a base unit for representing a game object.
 
-from tables import Table, newTable, hasKey, del, `[]`, `[]=`, add
+import tables
 
 
 type
@@ -9,6 +9,7 @@ type
 
 var
   entities: set[Entity] = {}  # set[int32] won't compile
+  # destructors: seq[proc(entity: Entity) {.closure.}]  # registered destructors which will be called on entity deletion
 
 
 # ---- Entity ----
@@ -22,14 +23,27 @@ proc newEntity*(): Entity =
 
 proc delete*(entity: Entity) =
   ## Delete the Entity
-  entities.excl(entity)  # will not alert if entity does not exist
   # TODO: delete all components related to this entity
+  # for destructor in destructors:
+  #   destructor(entity)
+  entities.excl(entity)  # will not alert if entity does not exist
 
 # ---- Components ----
 proc getComponents*(t: typedesc): ref Table[Entity, t] =
   ## Returns a table of components for all entities (`Table[Entity, t]`)
   var table {.global.} = newTable[Entity, t]()
   return table
+
+  # var table {.global.}: TableRef[Entity, t] 
+  # if table.isNil:
+  #   table = newTable[Entity, t]()
+  #   if destructors.isNil:  # this line may be called even earlier than `var` declarations of this file
+  #     destructors = @[]  # thus we need to init whatever we need
+  #   destructors.add(proc(entity: Entity) {.closure.} = echo "Destroyed " & t.name & " for entity " & $entity)
+  #   # destructors.add(proc(entity: Entity) {.closure.} = discard getComponents(t); echo "OK")
+  #   echo "Table just initialized for type " & $t.name
+  # return table
+
 
 template has*(entity: Entity, t: typedesc): bool = getComponents(t).hasKey(entity)
 template del*(entity: Entity, t: typedesc) = getComponents(t).del(entity)
