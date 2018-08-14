2a4a8fede49f69957896058e4da119b442fcd3d9
Alex Goncharov <alexandr@hobby4me.ru>
2018-08-14 13:10:23 +0300

    ECS: delete components when Entity is deleted

--- c4/core/entities.nim
+++ c4/core/entities.nim
@@ -3,13 +3,15 @@
 import tables
 import strformat
 import typetraits
+import logging
 
 import messages
 
 
 type
-  Entity* = int16  # uint doesn't check boundaries
-  ## Entity is just an int which may have components of any type.
+  Entity* = int16  ## Entity is just an int which may have components of any type.
+  # uint doesn't check boundaries
+
 
 var
   entities: set[Entity] = {}  # set[int32] won't compile
@@ -18,29 +20,38 @@ var
 
 # ---- Entity ----
 proc newEntity*(): Entity =
-  ## Return new Entity or raises error if limit exceeded
+  ## Return new Entity or raise error if limit exceeded
   result = low(Entity)
   while result in entities:
     result += 1  # TODO: pretty dumb
 
   entities.incl(result)  # add entity to global entities registry
 
-proc delete*(entity: Entity) =
-  ## Delete the Entity
-  # TODO: delete all components related to this entity
-  # for destructor in destructors:
-  #   destructor(entity)
-  entities.excl(entity)  # will not alert if entity does not exist
-
-proc flush*() =
-  ## Removes all entities
-  for entity in entities:
-    entity.delete()
-
 # ---- Components ----
+
+# Here goes a hacky code.
+# Entity may have as much components as desired. For each type (component) there is a separate table ``Table[Entity, <component type]``. This table is created automatically by compiler using {.global.} pragma.
+# However, we want to delete all components when Entity is deleted. Since components tables are created automatically, we don't have a list of tables to delete from. The only way to know which tables require deletion of components is to create a component destructor for that specific table when it is initialised, and add each destructor to a sequence.
+# When Entity is deleted, call all destructors from that sequence.
+# The idea is quite simple, but ``{.global.}`` variables have a bit complicated behaviour. In order tomake it working we need to make destructors sequence ``{.global.}`` too.
+
+type ComponentDestructor = proc(entity: Entity) {.nimcall.}
+
+proc getComponentDestructors(): var seq[ComponentDestructor] =
+  # Returns a sequence of destructors, one for each component
+  var destructors {.global.}: seq[ComponentDestructor] = @[]
+  return destructors
+
+proc getComponents*(t: typedesc): ref Table[Entity, t]
+
+proc newTableAndDestructor(t: typedesc): ref Table[Entity, t] =
+  # Creates a components table for specific type, as well as destructor proc for that type
+  result = newTable[Entity, t]()
+  getComponentDestructors().add(proc(entity: Entity) = getComponents(t).del(entity))  # TODO: is it safe to call `del` on non-existent key
+
 proc getComponents*(t: typedesc): ref Table[Entity, t] =
-  ## Returns a table of components for all entities (``Table[Entity, t]``)
-  var table {.global.} = newTable[Entity, t]()
+  ## Returns a table of components of specific type ``t`` (``Table[Entity, t]``)
+  var table {.global.} = newTableAndDestructor(t)
   return table
 
   # var table {.global.}: TableRef[Entity, t] 
@@ -52,6 +63,20 @@ proc getComponents*(t: typedesc): ref Table[Entity, t] =
   #   # destructors.add(proc(entity: Entity) {.closure.} = discard getComponents(t); echo "OK")
   #   echo "Table just initialized for type " & $t.name
   # return table
+
+proc delete*(entity: Entity) =
+  ## Delete the Entity and all its components
+  for destructor in getComponentDestructors():
+    destructor(entity)
+
+  if not entity in entities:
+    logging.warn &"Trying to delete entity {entity}, but it doesn't exist"
+  entities.excl(entity)  # will not alert if entity does not exist
+
+proc flush*() =
+  ## Removes all entities
+  for entity in entities:
+    entity.delete()
 
 
 template has*(entity: Entity, t: typedesc): bool = getComponents(t).hasKey(entity)
