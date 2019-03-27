99da1f94df542425f515854dcbefcb71c1dbb47d
Alex <alexandr@hobby4me.ru>
2019-03-27 21:33:02 +0300

    Try initializing Ogre3d

--- c4/lib/ogre3d/ogre3d.nim
+++ c4/lib/ogre3d/ogre3d.nim
@@ -13,24 +13,44 @@ else:
   {.link: "/usr/lib/libOgre.so".}
   {.link: "/usr/lib/libOgreMain.so".}
   {.passC: "-I/usr/include/OGRE".}  # -I/usr/include/OGRE/RTShaderSystem -I/usr/include/OGRE/Bites".}
+  const pluginsConfig = "/usr/share/OGRE/plugins.cfg"
 
 
 type
   String* {.header: "OgrePrerequisites.h", importcpp: "Ogre::String", bycopy.} = object
   Root* {.header: "OgreRoot.h", importcpp: "Ogre::Root", bycopy.} = object
+  RenderWindow* {.header: "OgreRenderWindow.h", importcpp: "Ogre::RenderWindow", bycopy.} = object
+  ConfigDialog* {.header: "OgreConfigDialog.h", importcpp: "Ogre::ConfigDialog", bycopy.} = object
 
-var
-  OGRE_BUILD_SUFFIX {.header: "OgrePlatform.h", importcpp: "OGRE_BUILD_SUFFIX".}: cstring
+
+# var
+#   OGRE_BUILD_SUFFIX {.header: "OgrePlatform.h", importcpp: "OGRE_BUILD_SUFFIX".}: cstring
 
 
-proc newRoot*(pluginFileName: cstring = &"plugins{OGRE_BUILD_SUFFIX}.cfg", configFileName: cstring = "ogre.cfg", logFileName: cstring = "Ogre.log"): ptr Root {.header: "OgreRoot.h", importcpp: "new Ogre::Root(@)", constructor.}
+proc newRoot*(
+  pluginFileName: cstring = pluginsConfig,
+  configFileName: cstring = "ogre.cfg",
+  logFileName: cstring = "ogre.log"
+): ptr Root {.header: "OgreRoot.h", importcpp: "new Ogre::Root(@)", constructor.}
+
+proc showConfigDialog(this: ptr Root, dialog: ptr ConfigDialog = nil): bool {.header: "OgreRoot.h", importcpp: "#.showConfigDialog(@)".}
+proc restoreConfig*(this: ptr Root): bool {.header: "OgreRoot.h", importcpp: "#.restoreConfig(@)".}
+
+# RenderWindow* initialise(bool autoCreateWindow, const String& windowTitle = "OGRE Render Window", const String& customCapabilitiesConfig = BLANKSTRING);
+proc initialise*(
+  this: ptr Root,
+  autoCreateWindow: bool,
+  windowTitle: cstring = "OGRE Render Window",
+  customCapabilitiesConfig: cstring = "",
+): ptr RenderWindow {.header: "OgreRoot.h", importcpp: "#.initialise(@)".}
 
 
 when isMainModule:
   suite "OGRE bindings test":
-    test "Const reading":
-      echo &"OGRE_BUILD_SUFFIX: '{OGRE_BUILD_SUFFIX}'"
-
     test "Base initialization":
-      discard newRoot(logFileName="test.log")
+      var root = newRoot(pluginFileName="plugins.cfg", configFileName="ogre.cfg", logFileName="ogre.log")
 
+      if not root.restoreConfig() and not root.showConfigDialog():
+        raise newException(LibraryError, "Could not load config")
+
+      discard root.initialise(false)
