0b86a81db8b968b68109661a26b15b2db7f7f4ec
Alex <alexandr@hobby4me.ru>
2019-03-31 22:14:51 +0300

    Order & update Ogre3d bindings

--- c4/lib/ogre3d/ogre3d.nim
+++ c4/lib/ogre3d/ogre3d.nim
@@ -28,15 +28,15 @@ type
   StdMap[K, V] {.header: "<map>", importcpp: "std::map".} = object
   String* {.header: "OgrePrerequisites.h", importcpp: "Ogre::String", bycopy.} = object
   # String* {.header: "<string>", importcpp: "std::string", bycopy.} = object
-  Root* {.header: "OgreRoot.h", importcpp: "Ogre::Root", bycopy.} = object
   RenderWindow* {.header: "OgreRenderWindow.h", importcpp: "Ogre::RenderWindow", bycopy.} = object
   ConfigDialog* {.header: "OgreConfigDialog.h", importcpp: "Ogre::ConfigDialog", bycopy.} = object
-  ResourceGroupManager* {.header: "OgreResourceGroupManager.h", importcpp: "Ogre::ResourceGroupManager", bycopy.} = object
   NameValuePairList* = StdMap[String, String]
 
+when defined(OGRE_DOUBLE_PRECISION):
+  type Real* = float64
+else:
+  type Real* = float32
 
-var
-  DEFAULT_RESOURCE_GROUP_NAME {.header: "OgreResourceGroupManager.h", importcpp: "Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME".}: cstring
 
 proc `[]=`*[K, V](this: var StdMap[K, V]; key: K; val: V) {.importcpp: "#[#] = #", header: "<map>".}
 proc toString*[T](value: T): String {.importcpp: "std::to_string(@)".}
@@ -44,18 +44,74 @@ proc c_str*(value: String): cstring {.importcpp: "#.c_str()".}
 proc initString*(value: cstring): String {.header: "OgrePrerequisites.h", importcpp: "Ogre::String(@)".}
 # proc initNameValuePairList*: NameValuePairList {.headerimportcpp: "NameValuePairList()".}
 
-proc newRoot*(pluginFileName: cstring = pluginsConfig, configFileName: cstring = "ogre.cfg", logFileName: cstring = "ogre.log"): ptr Root {.header: "OgreRoot.h", importcpp: "new Ogre::Root(@)", constructor.}
+# {.push header: "OgreVector.h".}
+
+# {.pop.}
+
+
 # proc newRenderWindow*(): ptr RenderWindow {.header: "OgreRenderWindow.h", importcpp: "new(Ogre::RenderWindow)", constructor.}
-proc createRenderWindow(this: ptr Root, name: cstring, width: uint, height: uint, fullScreen: bool, miscParams: ptr NameValuePairList = nil): ptr RenderWindow {.header: "OgreRoot.h", importcpp: "#.createRenderWindow(@)".}
+
+{.push header: "OgreResourceGroupManager.h".}
+type
+  ResourceGroupManager* {.importcpp: "Ogre::ResourceGroupManager", bycopy.} = object
+
+var
+  DEFAULT_RESOURCE_GROUP_NAME {.importcpp: "Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME".}: cstring
+
+proc getSingletonPtr*(): ptr ResourceGroupManager {.importcpp: "Ogre::ResourceGroupManager::getSingletonPtr()".}
+
+{.push importcpp: "#.$1(@)".}
+proc addResourceLocation*(this: ptr ResourceGroupManager, name: cstring, locType: cstring, resGroup: cstring = DEFAULT_RESOURCE_GROUP_NAME, recursive: bool = false, readOnly: bool = true)
+proc initialiseAllResourceGroups*(this: ptr ResourceGroupManager)
+{.pop.}
+{.pop.}
 
 
-proc showConfigDialog(this: ptr Root, dialog: ptr ConfigDialog = nil): bool {.header: "OgreRoot.h", importcpp: "#.showConfigDialog(@)".}
-proc restoreConfig*(this: ptr Root): bool {.header: "OgreRoot.h", importcpp: "#.restoreConfig(@)".}
+{.push header: "OgreCamera.h".}
+type
+  Camera* {.importcpp: "Ogre::Camera", bycopy.} = object
 
-proc initialise*(this: ptr Root, autoCreateWindow: bool, windowTitle: cstring = "OGRE Render Window", customCapabilitiesConfig: cstring = ""): ptr RenderWindow {.header: "OgreRoot.h", importcpp: "#.initialise(@)".}
-proc getSingletonPtr*(): ptr ResourceGroupManager {.header: "OgreResourceGroupManager.h", importcpp: "Ogre::ResourceGroupManager::getSingletonPtr()".}
+{.push importcpp: "#.$1(@)".}
+# TODO: @deprecated attach to SceneNode and use SceneNode::lookAt
+proc setPosition*(this: ptr Camera, x: Real, y: Real, z: Real)
+proc lookAt*(this: ptr Camera, x: Real, y: Real, z: Real)
+{.pop.}
+{.pop.}
 
-proc addResourceLocation*(this: ResourceGroupManager, name: cstring, locType: cstring, resGroup: cstring = DEFAULT_RESOURCE_GROUP_NAME, recursive: bool = false, readOnly: bool = true) {.header: "OgreResourceGroupManager.h", importcpp: "#.addResourceLocation(@)".}
+
+{.push header: "OgreSceneManager.h".}
+type
+  SceneManager* {.importcpp: "Ogre::SceneManager", bycopy.} = object
+  # /// Bitmask containing scene types
+  #   typedef uint16 SceneTypeMask;
+  # SceneType* = enum  # {.size: sizeof(cint).} = enum (probably uint16)
+  #   ST_GENERIC = 1,
+  #   ST_EXTERIOR_CLOSE = 2,
+  #   ST_EXTERIOR_FAR = 4,
+  #   ST_EXTERIOR_REAL_FAR = 8,
+  #   ST_INTERIOR = 16,
+
+{.push importcpp: "#.$1(@)".}
+proc createCamera*(this: ptr SceneManager, name: cstring): ptr Camera
+{.pop.}
+{.pop.}
+
+
+{.push header: "OgreRoot.h".}
+type
+  Root* {.importcpp: "Ogre::Root", bycopy.} = object
+
+proc newRoot*(pluginFileName: cstring = pluginsConfig, configFileName: cstring = "ogre.cfg", logFileName: cstring = "ogre.log"): ptr Root {.importcpp: "new Ogre::Root(@)", constructor.}
+
+{.push importcpp: "#.$1(@)".}
+proc showConfigDialog(this: ptr Root, dialog: ptr ConfigDialog = nil): bool
+proc restoreConfig*(this: ptr Root): bool
+proc initialise*(this: ptr Root, autoCreateWindow: bool, windowTitle: cstring = "OGRE Render Window", customCapabilitiesConfig: cstring = ""): ptr RenderWindow
+proc createRenderWindow*(this: ptr Root, name: cstring, width: uint, height: uint, fullScreen: bool, miscParams: ptr NameValuePairList = nil): ptr RenderWindow
+proc createSceneManager*(this: ptr Root): ptr SceneManager
+{.pop.}
+{.pop.}
+
 
 when isMainModule:
   suite "OGRE bindings test":
@@ -105,10 +161,10 @@ when isMainModule:
       # else:
       #   raise newException(LibraryError, "SDL video driver undefined")
 
-      echo ">>>"
-      echo $nativeWindowHandle
-      echo nativeWindowHandle.toString().c_str()
-      echo ">>>"
+      # echo ">>>"
+      # echo $nativeWindowHandle
+      # echo nativeWindowHandle.toString().c_str()
+      # echo ">>>"
 
       discard root.initialise(false)
 
@@ -117,11 +173,23 @@ when isMainModule:
 
       var renderWindow = root.createRenderWindow("Main Render Window", 800, 600, false, misc.addr)
 
-    # test "Loading resources":
-    #   var resourceManager: ptr ResourceGroupManager = getSingletonPtr()
-    #   resourceManager[].addResourceLocation(mediaDir / "packs" / "SdkTrays.zip", "Zip", resGroup="Essential")
+    test "Loading resources":
+      var resourceManager: ptr ResourceGroupManager = getSingletonPtr()
+      resourceManager.addResourceLocation(mediaDir / "packs" / "SdkTrays.zip", "Zip", resGroup="Essential")
 
-    #   resourceManager[].addResourceLocation(mediaDir, "FileSystem", resGroup="General")
-    #   resourceManager[].addResourceLocation(mediaDir / "models", "FileSystem", resGroup="General")
-    #   resourceManager[].addResourceLocation(mediaDir / "materials" / "scripts", "FileSystem", resGroup="General")
-    #   resourceManager[].addResourceLocation(mediaDir / "materials" / "textures", "FileSystem", resGroup="General")
+      resourceManager.addResourceLocation(mediaDir, "FileSystem", resGroup="General")
+      resourceManager.addResourceLocation(mediaDir / "models", "FileSystem", resGroup="General")
+      resourceManager.addResourceLocation(mediaDir / "materials" / "scripts", "FileSystem", resGroup="General")
+      resourceManager.addResourceLocation(mediaDir / "materials" / "textures", "FileSystem", resGroup="General")
+
+      # Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
+
+      resourceManager.initialiseAllResourceGroups()
+
+    test "Scene creation":
+      var
+        sceneManager = root.createSceneManager()
+        camera = sceneManager.createCamera("camera")
+
+      camera.setPosition(0.0, 0.0, 80.0)
+      camera.lookAt(0, 0, -300)
