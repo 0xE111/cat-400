62f8eb1080266e90cf9ff758f52dc1b78e7efea5
Alex <alexandr@hobby4me.ru>
2019-03-31 18:56:26 +0300

    Ogre3d String crap

--- c4/lib/ogre3d/ogre3d.nim
+++ c4/lib/ogre3d/ogre3d.nim
@@ -4,6 +4,10 @@ import unittest
 import strformat
 import os
 
+when isMainModule:
+  # some modules required for unit testing
+  import sdl2/sdl, sdl2/sdl_syswm
+
 when defined(windows):
   raise newException(LibraryError, "Not implemented")
 
@@ -21,18 +25,29 @@ else:
 
 
 type
+  StdMap[K, V] {.header: "<map>", importcpp: "std::map".} = object
   String* {.header: "OgrePrerequisites.h", importcpp: "Ogre::String", bycopy.} = object
+  # String* {.header: "<string>", importcpp: "std::string", bycopy.} = object
   Root* {.header: "OgreRoot.h", importcpp: "Ogre::Root", bycopy.} = object
   RenderWindow* {.header: "OgreRenderWindow.h", importcpp: "Ogre::RenderWindow", bycopy.} = object
   ConfigDialog* {.header: "OgreConfigDialog.h", importcpp: "Ogre::ConfigDialog", bycopy.} = object
   ResourceGroupManager* {.header: "OgreResourceGroupManager.h", importcpp: "Ogre::ResourceGroupManager", bycopy.} = object
+  NameValuePairList* = StdMap[String, String]
 
 
 var
   DEFAULT_RESOURCE_GROUP_NAME {.header: "OgreResourceGroupManager.h", importcpp: "Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME".}: cstring
 
+proc `[]=`*[K, V](this: var StdMap[K, V]; key: K; val: V) {.importcpp: "#[#] = #", header: "<map>".}
+proc toString*[T](value: T): String {.importcpp: "std::to_string(@)".}
+proc c_str*(value: String): cstring {.importcpp: "#.c_str()".}
+proc initString*(value: cstring): String {.header: "OgrePrerequisites.h", importcpp: "Ogre::String(@)".}
+# proc initNameValuePairList*: NameValuePairList {.headerimportcpp: "NameValuePairList()".}
 
 proc newRoot*(pluginFileName: cstring = pluginsConfig, configFileName: cstring = "ogre.cfg", logFileName: cstring = "ogre.log"): ptr Root {.header: "OgreRoot.h", importcpp: "new Ogre::Root(@)", constructor.}
+# proc newRenderWindow*(): ptr RenderWindow {.header: "OgreRenderWindow.h", importcpp: "new(Ogre::RenderWindow)", constructor.}
+proc createRenderWindow(this: ptr Root, name: cstring, width: uint, height: uint, fullScreen: bool, miscParams: ptr NameValuePairList = nil): ptr RenderWindow {.header: "OgreRoot.h", importcpp: "#.createRenderWindow(@)".}
+
 
 proc showConfigDialog(this: ptr Root, dialog: ptr ConfigDialog = nil): bool {.header: "OgreRoot.h", importcpp: "#.showConfigDialog(@)".}
 proc restoreConfig*(this: ptr Root): bool {.header: "OgreRoot.h", importcpp: "#.restoreConfig(@)".}
@@ -40,18 +55,73 @@ proc restoreConfig*(this: ptr Root): bool {.header: "OgreRoot.h", importcpp: "#.
 proc initialise*(this: ptr Root, autoCreateWindow: bool, windowTitle: cstring = "OGRE Render Window", customCapabilitiesConfig: cstring = ""): ptr RenderWindow {.header: "OgreRoot.h", importcpp: "#.initialise(@)".}
 proc getSingletonPtr*(): ptr ResourceGroupManager {.header: "OgreResourceGroupManager.h", importcpp: "Ogre::ResourceGroupManager::getSingletonPtr()".}
 
-proc addResourceLocation*(this: ptr ResourceGroupManager, name: cstring, locType: cstring, resGroup: cstring = DEFAULT_RESOURCE_GROUP_NAME, recursive: bool = false, readOnly: bool = true) {.header: "OgreResourceGroupManager.h", importcpp: "#.addResourceLocation(@)".}
+proc addResourceLocation*(this: ResourceGroupManager, name: cstring, locType: cstring, resGroup: cstring = DEFAULT_RESOURCE_GROUP_NAME, recursive: bool = false, readOnly: bool = true) {.header: "OgreResourceGroupManager.h", importcpp: "#.addResourceLocation(@)".}
 
 when isMainModule:
   suite "OGRE bindings test":
-    test "Base initialization":
-      var root = newRoot()
+    var root = newRoot()
+    test "Configuration":
+      # TODO:
+      # proc getAvailableRenderers*(this: ptr Root):
+      # Once you have a pointer to the RenderSystem, you can use RenderSystem::getConfigOptions to see what options it provides.
+      # RenderSystem* rs = mRoot->getRenderSystemByName("Direct3D9 Rendering Subsystem");
+
+      # mRoot->setRenderSystem(rs);
+      # rs->setConfigOption("Full Screen", "No");
+      # rs->setConfigOption("Video Mode", "800 x 600 @ 32-bit colour");
 
       if not root.restoreConfig() and not root.showConfigDialog():
         raise newException(LibraryError, "Could not load config")
 
+    test "Init SDL+Ogre3d":
+      if sdl.initSubSystem(sdl.INIT_VIDEO) != 0:
+        raise newException(LibraryError, "Could not init SDL video subsystem")
+
+      var window = sdl.createWindow(
+        "Ogre3d test", 100, 100, 800, 600,
+        (sdl.WINDOW_SHOWN or sdl.WINDOW_OPENGL or sdl.WINDOW_RESIZABLE).uint32,  # TODO: do we need WINDOW_OPENGL for other backends?
+      )
+      if window == nil:
+        raise newException(LibraryError, "Could not create SDL window")
+
+      if sdl.setRelativeMouseMode(true) != 0:
+        raise newException(LibraryError, "Could not enable relative mouse mode")
+
+      # get native window handle
+      var info: sdl_syswm.SysWMinfo
+      sdl.version(info.version)
+      assert sdl_syswm.getWindowWMInfo(window, info.addr)
+      var nativeWindowHandle = info.info.x11.window  # culong
+
+      # when defined(SDL_VIDEO_DRIVER_WINDOWS):
+      #   nativeWindowHandle = cast[pointer](info.info.win.window)
+
+      # elif defined(SDL_VIDEO_DRIVER_X11):
+      #   nativeWindowHandle = cast[pointer](info.info.x11.window)
+
+      # elif defined(SDL_VIDEO_DRIVER_COCOA):
+      #   nativeWindowHandle = cast[pointer](info.info.cocoa.window)
+
+      # else:
+      #   raise newException(LibraryError, "SDL video driver undefined")
+
+      echo ">>>"
+      echo $nativeWindowHandle
+      echo nativeWindowHandle.toString().c_str()
+      echo ">>>"
+
       discard root.initialise(false)
 
-    test "Loading resources":
-      var resourceManager: ptr ResourceGroupManager = getSingletonPtr()
-      resourceManager.addResourceLocation(mediaDir / "packs" / "SdkTrays.zip", "Zip")
+      var misc: NameValuePairList
+      misc[initString("externalWindowHandle")] = nativeWindowHandle.toString()
+
+      var renderWindow = root.createRenderWindow("Main Render Window", 800, 600, false, misc.addr)
+
+    # test "Loading resources":
+    #   var resourceManager: ptr ResourceGroupManager = getSingletonPtr()
+    #   resourceManager[].addResourceLocation(mediaDir / "packs" / "SdkTrays.zip", "Zip", resGroup="Essential")
+
+    #   resourceManager[].addResourceLocation(mediaDir, "FileSystem", resGroup="General")
+    #   resourceManager[].addResourceLocation(mediaDir / "models", "FileSystem", resGroup="General")
+    #   resourceManager[].addResourceLocation(mediaDir / "materials" / "scripts", "FileSystem", resGroup="General")
+    #   resourceManager[].addResourceLocation(mediaDir / "materials" / "textures", "FileSystem", resGroup="General")
