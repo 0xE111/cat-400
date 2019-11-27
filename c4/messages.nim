b052caaf729351668e50e0c7555437ff6e4e4e79
c0ntribut0r <a@hobby4me.ru>
2019-11-28 00:23:34 +0300

    Make messages multithreaded

--- c4/messages.nim
+++ c4/messages.nim
@@ -68,12 +68,18 @@ method packId*(self: ref Message): uint8 {.base.} =
   raise newException(LibraryError, "Trying to pack/unpack base Message type")
 method `$`*(self: ref Message): string {.base.} = "Message"
 
-proc pack*(message: ref Message): string =
+proc pack*(message: ref Message): string {.gcsafe.} =
   ## General method which selects appropriate pack method from pack table according to real message runtime type.
-  withLock packTableLock:
-    result = packTablePtr[][message.packId].pack(message)
+  # var packProc: PackProc
+  # withLock packTableLock:
+  #   packProc = packTablePtr[][message.packId].pack
+  # result = packProc(message)
 
-proc unpack*(data: string): ref Message =
+  {.gcsafe.}:  # im so sorry for this
+    withLock packTableLock:
+      result = packTablePtr[][message.packId].pack(message)
+
+proc unpack*(data: string): ref Message {.gcsafe.} =
   ## General method which selects appropriate unpack method from pack table according to real message runtime type.
   var
     packId: uint8
@@ -81,8 +87,9 @@ proc unpack*(data: string): ref Message =
 
   stream.setPosition(0)
   stream.unpack(packId)
-  withLock packTableLock:
-    result = packTablePtr[][packId].unpack(stream)
+  {.gcsafe.}:
+    withLock packTableLock:
+      result = packTablePtr[][packId].unpack(stream)
 
 # -- Message subtype --
 template register*(MessageType: typedesc) =
@@ -98,7 +105,7 @@ template register*(MessageType: typedesc) =
         var stream = MsgStream.init(sizeof(packId) + sizeof(MessageType))
 
         stream.pack packId
-        stream.pack (ref MessageType)message
+        stream.pack (ref MessageType) message
 
         result = stream.data,
 
@@ -110,6 +117,7 @@ template register*(MessageType: typedesc) =
 
   method packId*(self: ref MessageType): uint8 = messageId
   method `$`*(self: ref MessageType): string = $(self[].type) & $self[]
+  proc pack*(self: ref MessageType): string = pack((ref Message)self)  # required for instant pack
 
 
 when isMainModule:
@@ -130,40 +138,48 @@ when isMainModule:
 
   suite "Messages test":
     var
-      message: ref Message
-      messageA: ref MessageA
-      messageB: ref MessageB
-
-    var
       packed: string
       unpacked: ref Message
 
-    new(message)
-    new(messageA)
-    messageA.msg = "some message"
-    messageA.sender = new(ref Peer)
-    new(messageB)
-    messageB.counter = 42
-    messageB.data = "some data string"
-    messageB.is_correct = true
-
     test "Pack/unpack base Message type":
       expect LibraryError:
-        packed = pack(message)
+        packed = pack(new(Message))
 
     test "Pack/unpack Message subtypes":
-      message = messageA
+      var message: ref Message
+
+      message = (ref MessageA)(msg: "some message")
       packed = pack(message)
-      # echo "MessageA packed as: " & stringify(packed)
+      echo "MessageA packed as: " & stringify(packed)
       unpacked = packed.unpack()
+
       check:
         packed.len == 17
         unpacked.getData() == "some message"
 
-      message = messageB
+      message = (ref MessageB)(counter: 42, data: "some data string", is_correct: true)
       packed = pack(message)
-      # echo "MessageB packed as: " & stringify(packed)
+      echo "MessageB packed as: " & stringify(packed)
       unpacked = packed.unpack()
       check:
         packed.len == 23
         unpacked.getData() == "42"
+
+    test "Instant pack":
+      let packedInstant = pack((ref MessageB)(counter: 42))
+      echo "Instant packed as: " & stringify(packedInstant)
+
+      var msg: ref Message = (ref MessageB)(counter: 42)
+      let packedVariable = pack(msg)
+      echo "Var packed as: " & stringify(packedVariable)
+
+      check:
+        packedInstant == packedVariable
+
+    test "Inside a thread":
+      var thread: Thread[void]
+
+      thread.createThread(tp = proc() {.thread.} =
+        let packed = pack((ref MessageB)(counter: 42))
+        discard packed.unpack()
+      )
